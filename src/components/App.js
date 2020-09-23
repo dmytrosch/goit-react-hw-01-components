d8938292d6f9c3fa63fd18bb0f51c603e65ebcd2
Dmytro.Sch <dmytro.sch@list.ru>
2020-09-23 20:33:52 +0300

    task 2 ended

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 import Profile from "./Profile/Profile";
 import user from "../data/user.json";
+import Statistics from "./Statistics/Statistics";
+import statisticalData from "../data/statistical-data.json";
 
-ReactDom.render(
-    <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-    />,
-    document.querySelector("#root")
+const mainRootRef = document.querySelector("#root");
+
+// ReactDom.render(
+//     <Profile
+//         name={user.name}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats={user.stats}
+//     />,
+//     mainRootRef
+// );
+
+ReactDOM.render(
+    <Statistics title="Upload stats" stats={statisticalData} />,
+    mainRootRef
 );
+
+
