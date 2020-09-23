5510a763b0dc62ae1773f5b42e6e805ad4f1d0ed
Dmytro.Sch <dmytro.sch@list.ru>
2020-09-23 22:45:08 +0300

    Updates

---
+++ precache-manifest.b26d357abbb928b09b81e92db330a79c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c54d45cd56de47d7a46d1bc67955a2",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "a6f2062cd7bc3aea6c6d",
+    "url": "/goit-react-hw-01-components/static/css/main.213c03ac.chunk.css"
+  },
+  {
+    "revision": "8702e87f2de9d9d43916",
+    "url": "/goit-react-hw-01-components/static/js/2.f39d56b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.f39d56b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f2062cd7bc3aea6c6d",
+    "url": "/goit-react-hw-01-components/static/js/main.73c1d607.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
